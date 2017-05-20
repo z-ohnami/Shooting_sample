3ada918479c16728d3c5246b447a5d8e782af26f
Makoto Ohnami <z.ohnami@gmail.com>
2017-05-20 15:52:05 +0900

    player and enemies

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+	public int speed = 10;
+	// ゲームオブジェクト生成から削除するまでの時間
+	public float lifeTime = 5;
+
+	void Start ()
+	{
+		GetComponent<Rigidbody2D>().velocity = transform.up.normalized * speed;
+		// lifeTime秒後に削除
+		Destroy (gameObject, lifeTime);
+	}
+}
